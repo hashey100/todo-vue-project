6616a7efc34e33c66d9d3c465489fb0faf6394cb
Hesham Saied <heshamsaied@iMac.local>
2017-06-26 09:31:38 +0100

    bug fix

--- README.md
+++
@@ -1,6 +0,0 @@
-# Todo List Application using Vue.js
-
-I created a todo list using Vue.js
-
-Simple app which uses localstorage storing an array whenever the user removes or adds a message.
-
--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-function readData(){
-  var itemList = localStorage.getItem('todo');
-    return JSON.parse(itemList);
-  }
-
-var updatedlist = readData();
-
-if(updatedlist==null)
-{
-  updatedlist = [];
+function readData() {
+    var itemList = JSON.parse(localStorage.getItem('todo'));
+    if(itemList==null) {
+        itemList = [];
+    }
+    return itemList;
 }
 
 var app = new Vue({
   el: '#container',
   data: {
-    todos: updatedlist,
+    todos: readData(),
      input: ''
   },
   methods: {
     addMessage: function () {
-
-      if(this.input!='')
-      {
+      if(this.input!='') {
         app.todos.push({ text: this.input })
         localStorage.setItem( 'todo', JSON.stringify(this.todos) );
         var val = localStorage.getItem('todo');
       }
     },
-    removeMessage: function (todo) {
-      this.todos.splice(todo, 1);
-      console.log(todo);
+    removeMessage: function (key) {
+      this.todos.splice(key, 1);
       localStorage.setItem( 'todo', JSON.stringify(this.todos) );
     }
   }
